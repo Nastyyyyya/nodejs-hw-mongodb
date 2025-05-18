71f8b152372c9560a9e2e5325e018011f24a1fba
Anastasia Paslavska <a.paslavska2206@gmail.com>
2025-05-18 13:42:59 +0300

    change errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
-export const errorHandler = (err, res) => {
+// eslint-disable-next-line no-unused-vars
+export const errorHandler = (err, req, res, next) => {
   const { status = 500, message = 'Something went wrong' } = err;
   res.status(status).json({
     status,
